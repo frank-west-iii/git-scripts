6e1136576202f5a086c21b5aa008921f2d0e577b
Frank West <frank.west.iii@gmail.com>
2022-01-19 07:48:54 -0800

    Create git-branches.sh

---
+++ git-branches.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function listBranchWithDescription() {
+  branches=`git for-each-ref --format='%(committerdate:iso8601) %(committerdate:relative) %(refname)' --sort -committerdate refs/heads/`
+
+  # requires git > v.1.7.9
+
+  # you can set branch's description using command
+  # git branch --edit-description
+
+  # you can see branch's description using
+  # git config branch.<branch name>.description
+
+  while read -r branch; do
+    clean_branch_name=`echo $branch | sed -E "s/.*refs\/heads\///g"`
+    branch=`echo $branch | sed -E "s/refs\/heads\///g"`
+
+    description=`git config branch.$clean_branch_name.description`
+    printf "%-80s %s\n" "$branch" "$description" 
+  done <<< "$branches"
+}
+
+listBranchWithDescription "$@"
